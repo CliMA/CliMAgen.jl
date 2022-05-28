8f97b9a02883fecd325c719114315a099ba4241e
bischtob <tobias.bischoff@pm.me>
2022-05-28 16:17:36 -0700

    gpu version runs

--- examples/better_cyclegan.jl
+++ examples/better_cyclegan.jl
@@ -14,13 +14,13 @@ FT = Float32
 exp_name = "horse2zebra"
 input_path = "../data/"
 output_path = "./output/"
-eval_freq = 1
+eval_freq = 100
 checkpoint_freq = eval_freq
 device = gpu
-num_examples = 128
-num_epochs = 2
-batch_size = 4
-img_size = 28
+num_examples = 1000
+num_epochs = 100
+batch_size = 1
+img_size = 256
 input_channels = 3
 dis_lr = FT(0.0001)
 gen_lr = FT(0.0001)
@@ -103,7 +103,6 @@ function training()
         for (batch_idx, (a, b)) in enumerate(data)
             a, b = normalize(a), normalize(b)
             g_loss, d_loss = train_step(opt_gen, opt_dis, a, b)
-            total_loss = g_loss + d_loss
             total_iters += batch_size
 
             if total_iters % eval_freq == 0
@@ -112,11 +111,9 @@ function training()
 
             if total_iters % checkpoint_freq == 0
                 @info "Total iteration: $total_iters - Checkpointing model."
-                file_iter = output_path * exp_name * "/checkpoint_iteration_$(total_iters)_loss_$(total_loss).bson"
                 file_last = output_path * exp_name * "/checkpoint_latest.bson"
                 networks_cpu = networks |> cpu
-                @save file_iter networks_cpu
-                cp(file_iter, file_last, force=true)
+                @save file_last networks_cpu
             
                 prefix_path = output_path * exp_name * "/training/" * "image_$(total_iters)"
                 save_model_samples(prefix_path, a, b)
