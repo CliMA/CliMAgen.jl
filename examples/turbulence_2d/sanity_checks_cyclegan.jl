611152bdff5b8037af8d08acda33ff3483280c32
kmdeck <kdeck@caltech.edu>
2022-08-25 17:13:53 -0700

    early wip

--- examples/turbulence_2d/sanity_checks_cyclegan.jl
+++ examples/turbulence_2d/sanity_checks_cyclegan.jl
@@ -2,6 +2,9 @@ using BSON
 using Flux
 using FFTW
 using UnicodePlots
+using FFTW
+using Base.Iterators
+using DataFrames
 
 using Downscaling
 
@@ -27,3 +30,39 @@ y2 = generator(a, noise)[:, :, 1, 1];
 heatmap(x, width=64)
 heatmap(y1, width=64)
 heatmap(y2, width=64)
+
+
+mpty_matrix = Matrix{Float64}(undef, 64,64)
+empty_matrix  .= 0.0
+mean_fft = copy(empty_matrix )
+mean_fft_squared =  copy(empty_matrix)
+for i in 1:10
+    (elem, data) = Base.Iterators.peel(data)
+
+    (_, b, _) = elem
+    spectrum = abs.(fft(b .- mean(b))[1:64,1:64,1,1])
+    mean_fft .+= spectrum
+    mean_fft_squared .+= spectrum.^2.0
+end
+mean_fft = mean_fft ./ 10.0
+std_fft = sqrt.(mean_fft_squared ./10.0 - mean_fft.^2.0)
+std_error_fft = std_fft ./sqrt(10.0)
+
+amplitude = log10.(mean_fft[1:64, 1:64,1,1] .+ eps(1.0))[2:end]
+values = copy(empty_matrix)
+for i in 1:64
+    for j in 1:64
+        values[i,j] = log10(sqrt((i-1)^2.0+(j-1.0)^2.0) +eps(1.0))
+    end
+end
+values = values[2:end]
+radii = Array(0.0:0.05:1.95)
+bins = [argmin(abs.(radius .- radii)) for radius in values]
+df = DataFrame(amplitude = amplitude, radius_bin = bins, radius = values)
+gdf = groupby(df, :radius_bin)
+counts = combine(gdf, nrow)
+means = combine(gdf, [:amplitude, :radius] => ((p, s) -> (amplitude=mean(p), radius=mean(s))) => AsTable)
+stds = combine(gdf, [:amplitude, :radius] => ((p, s) -> (amplitude=std(p), radius=std(s))) => AsTable)
+std_errors = stds.amplitude ./ sqrt.(counts.nrow)
+std_errors[isnan.(std_errors)] .= means.amplitude[isnan.(std_errors)]
+plot(means.radius, means.amplitude, ribbon = std_errors, xlabel = "|k|", ylabel = "P(k)", label = "")
