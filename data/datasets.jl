7a1c1d484c1b8fcc9ff8fa8f428df8ab879b6ec5
bischtob <tobias.bischoff@pm.me>
2022-09-02 16:25:52 -0700

    diff model

--- data/datasets.jl
+++ data/datasets.jl
@@ -6,6 +6,12 @@ Base.@kwdef struct Turbulence2D <: ArtifactMetaData
     url = "https://caltech.box.com/shared/static/7oht5betdza54sjftk93ni0bsldtrptl.hdf5"
 end
 
+Base.@kwdef struct Turbulence2DComplete <: ArtifactMetaData
+    filename = "turbulence2d_nx512_ny512.hdf5"
+    dataname = "turbulence2d_nx512_ny512."
+    url = "https://caltech.box.com/shared/static/0golc3ynh76v0lnv25xk2dsnxvpgacav.hdf5"
+end
+
 Base.@kwdef struct KuramotoSivashinsky <: ArtifactMetaData
     filename = "kuramoto_sivashinsky.hdf5"
     dataname = "kuramoto_sivashinsky"
