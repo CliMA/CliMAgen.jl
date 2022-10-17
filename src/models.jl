fcee9e488c29f8a8d2e5f874e814570d1e1cfb55
Katherine M. Deck <kdeck@caltech.edu>
2022-10-17 11:21:59 -0700

    other sigma sched

--- src/models.jl
+++ src/models.jl
@@ -77,6 +77,6 @@ end
 
 function marginal_prob(m::VarianceExplodingSDE, x_0, t)
     μ_t = x_0
-    σ_t = @. m.σ_min * (m.σ_max/m.σ_min)^t
+    σ_t = @. sqrt(m.σ_min^2 * (m.σ_max/m.σ_min)^(2*t) - m.σ_min^2)
     return μ_t, expand_dims(σ_t, ndims(μ_t) - 1)
 end
