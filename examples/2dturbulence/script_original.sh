c1ddbbea7476aebaaafead14769124f0ee14ddc5
Katherine M. Deck <kdeck@caltech.edu>
2023-01-27 13:47:02 -0800

    scripts for color shift ablation

---
+++ examples/2dturbulence/script_original.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1 # number of MPI ranks per node
+#SBATCH --gres=gpu:1    # GPUs per node; should equal tasks-per-node
+#SBATCH --time=12:00:00
+set -euo pipefail # kill the job if anything fails
+set -x # echo script
+module purge
+module load julia/1.8.0 hdf5/1.12.1 cuda/11.2 openmpi/4.1.1_cuda-11.2 # CUDA-aware MPI
+export JULIA_NUM_THREADS=${SLURM_GPUS_PER_TASK:=1}
+export JULIA_MPI_BINARY=system
+export JULIA_CUDA_USE_BINARYBUILDER=false
+julia --project -e 'using Pkg; Pkg.instantiate(); Pkg.build()'
+julia --project -e 'using Pkg; Pkg.precompile()'
+julia --project -e 'using CUDA; @info CUDA.has_cuda()'
+#mpiexec julia --project analysis.jl $1
+mpiexec julia --project training_original.jl $1
+mpiexec julia --project analysis.jl $1
+echo done
