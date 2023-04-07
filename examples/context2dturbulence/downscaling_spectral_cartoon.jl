f1a1d2445940202003f39a3b5142220ae2fb926c
Katherine M. Deck <kdeck@caltech.edu>
2023-04-07 16:03:37 -0700

    wip

--- examples/context2dturbulence/downscaling_spectral_cartoon.jl
+++ examples/context2dturbulence/downscaling_spectral_cartoon.jl
@@ -21,7 +21,7 @@ function unpack_experiment(experiment_toml, wavenumber; device = Flux.gpu, FT=Fl
     savedir = params.experiment.savedir
     batchsize = params.data.batchsize
     resolution = params.data.resolution
-    fraction::FT = params.data.fraction
+    fraction::FT = 0.05#params.data.fraction
     standard_scaling  = params.data.standard_scaling
     preprocess_params_file = joinpath(savedir, "preprocessing_standard_scaling_$standard_scaling.jld2")
     scaling = JLD2.load_object(preprocess_params_file)
@@ -118,7 +118,7 @@ function vary_t0(wavenumber;
     target_samples = invert_preprocessing(cpu(target_samples), scaling_target);
     plot_array = []
     for t in end_times
-        push!(plot_array, Plots.plot(title = "t = $t", border = :none, ticks =nothing,size = (400,50), titlefontsize = 32))
+        push!(plot_array, Plots.plot(title = "t⋆= $t", border = :none, ticks =nothing,size = (400,50), titlefontsize = 32))
     end
 
     for ch in 1:noised_channels
@@ -136,14 +136,113 @@ function vary_t0(wavenumber;
 
     source_spectra, k = new_batch_spectra((xsource |> cpu)[:,:,:,1:20])
     target_spectra, k = new_batch_spectra((xtarget |> cpu)[:,:,:,1:20])
-    cutoff_idx = min(3,Int64(floor(sqrt(2)*wavenumber-1)))
-    if cutoff_idx > 0
+    values = [0.01,0.2, 3, 50, 1000]
+    times = [0,0.25,0.5,0.75,1]
+    names = ["Moisture", "Vorticity"]
+    plot_array = []
+    for ch in 1:noised_channels
+        plt = Plots.plot()
+        Plots.plot!(plt,k, source_spectra[:,ch,1], label = "Low-res",linewidth=3)
+        Plots.plot!(plt, k, target_spectra[:,ch,1], label = "High-res",linewidth=3)
+        for i in 1:length(values)
+            v = values[i]
+            t = times[i]
+            white_noise = randn(512,512);
+            noisy_spectra, k = new_power_spectrum2d(white_noise*v)
+            Plots.plot!(plt, k, noisy_spectra, label = "σ($t)=$v",linewidth=3)
+        end
+        Plots.plot!(plt, margins = 20Plots.mm, guidefontsize=12, titlefontsize = 24, tickfontsize = 8, legend =:topright, xlabel = "Wavenumber", ylabel = "Power Spectral Density", yaxis = :log, title = names[ch])
+        push!(plot_array, plt)
+    end
+    Plots.plot(plot_array..., layout = (1,2), size = (1400,700))
+    Plots.savefig("./psd_white_noise.png")
+end
+
+
+
+
+function two_model_bridge(;
+    source_toml="experiments/Experiment_resize_64_031422.toml",
+    target_toml="experiments/Experiment_preprocess_mixed_shorter_run_all_data.toml")
+FT = Float32
+device = Flux.gpu
+nsteps = 125
+savepath = "./output"
+tilesize = 512
+context_channels = 1
+noised_channels = 2
+
+forward_model, xsource, csource, scaling_source = unpack_experiment(source_toml, 0; device = device, FT=FT)
+reverse_model, xtarget, ctarget, scaling_target = unpack_experiment(target_toml, 0; device = device,FT=FT)
+
+# Determine which `k` the two sets of images begin to differ from each other
+source_spectra, k = new_batch_spectra((xsource |> cpu)[:,:,:,1:10])
+target_spectra, k = new_batch_spectra((xtarget |> cpu)[:,:,:,1:10])
+cutoff_idx = 3
+if cutoff_idx > 0
     k_cutoff = FT(k[cutoff_idx])
     target_power_at_cutoff = FT(mean(target_spectra[cutoff_idx,:,:]))
+    source_power_at_cutoff = FT(mean(source_spectra[cutoff_idx,:,:]))
+
     reverse_t_end = FT(new_t_cutoff(target_power_at_cutoff, k_cutoff, FT(512), reverse_model.σ_max, reverse_model.σ_min))
-    else
+    forward_t_end = FT(new_t_cutoff(source_power_at_cutoff, k_cutoff, FT(512), reverse_model.σ_max, reverse_model.σ_min))
+
+else
     reverse_t_end = FT(1)
+    forward_t_end = FT(1)
+end
+
+
+# create a bridge plot for a two different contexts with the same source
+idx = [10,55]
+init_x = cat(xsource[:,:,:,[1]],xsource[:,:,:,[1]], dims = 4)
+reverse_context = ctarget[:,:,:,idx]
+forward_context = cat(csource[:,:,:,[1]],csource[:,:,:,[1]], dims = 4)
+forward_solution, reverse_solution = diffusion_bridge_simulation(forward_model,
+                                               reverse_model,
+                                               init_x,
+                                               nsteps;
+                                               forward_c=forward_context,
+                                               reverse_c=reverse_context,
+                                               FT = FT,
+                                               forward_t_end=forward_t_end,
+                                               forward_solver=DifferentialEquations.EM(),
+                                               forward_sde=true,
+                                               reverse_solver=DifferentialEquations.EM(),
+                                               reverse_t_end=reverse_t_end,
+                                               reverse_sde=true
+                                               )
+# We need to scale each one to have the same range. 
+images = cat(forward_solution.u..., reverse_solution.u[2:end]..., dims = (4));
+maxes = maximum(images, dims = (1,2))
+mins = minimum(images, dims = (1,2))
+images = @. (images - mins) / (maxes - mins)
+images = cpu(images)
+#times = cat(forward_solution.t..., reverse_solution.t[2:end]...,dims = 1);
+times = [0,0.06,0.24,0.54,0.24,0.6,0]
+ntimes = length(times)
+plot_array = []
+    for t in times
+        push!(plot_array, Plots.plot(title = "t = $t", border = :none, ticks =nothing,size = (400,50), titlefontsize = 32))
     end
+    clims = (minimum(images[:,:,1,:]), maximum(images[:,:,1,:]))
+    for i in 1:ntimes
+        t = times[i]
+        plt = Plots.plot()
+        Plots.heatmap!(plt, images[:,:,1,2*(i-1)+1],clims = clims,size = (400,400))
+        push!(plot_array,plt)
+    end
+    for i in 1:ntimes
+        t = times[i]
+        plt = Plots.plot()
+        Plots.heatmap!(plt, images[:,:,1,i*2],clims = clims,size = (400,400))
+        push!(plot_array,plt)
+    end
+
+    heights = [0.03, 0.485,0.485]
+    Plots.plot(plot_array..., layout= grid(3,7, heights = heights), size = (2800,850), colorbar = :none, border = :none, ticks= nothing)
+    Plots.savefig("./downscale2.png")
+
 end
 
 
