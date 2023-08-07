b9e2caba7cf6710f556e50ab2051678b722a35f8
kmdeck <kdeck@caltech.edu>
2023-08-07 15:43:25 -0700

    runs

--- examples/correlated_ou/analysis_windowsize.jl
+++ examples/correlated_ou/analysis_windowsize.jl
@@ -80,41 +80,44 @@ function run_analysis(params; FT=Float32, logger=nothing)
         num_images=nsamples,
         num_steps=nsteps,
     )
+    # Generated samples should be already the timeseries
     samples = CliMAgen.Euler_Maruyama_timeseries_sampler(model, init_x, time_steps, Δt; c=ctest[:,:,:,[1]])
-    samples = reshape(samples, (resolution, resolution, 1, nsamples*noised_channels))[:,:,:,1:nsamples]
-    timeseries = reshape(cat(xtest[:,:,:,1:nsamples], ctest[:,:,:,1:nsamples], dims = 3), (resolution, resolution, 1, nsamples*(noised_channels+context_channels)))[:,:,:,1:nsamples]
+    # reshape into a timeseries, time in the batch dimension, channels = 1
+    gen_timeseries = reshape(samples, (resolution, resolution, 1, nsamples*noised_channels))[:,:,:,1:nsamples]
+    # the data timeseries
+    data_timeseries = reshape(test[:,:,:,1:nsamples], (resolution, resolution, 1, nsamples*(noised_channels+context_channels)))[:,:,:,1:nsamples]
    
     dt_save = 1.0
-    ac, lag, npairs = autocorr(samples, 1, 16, 16)
+    ac, lag, npairs = autocorr(gen_timeseries, 1, 16, 16)
     ac_l = autocorr_inverse_cdf.(0.05, npairs, ac)
     ac_up = autocorr_inverse_cdf.(0.95, npairs, ac)
     Plots.plot(lag*dt_save, ac,  ribbon = (ac .- ac_l, ac_up .- ac), label = "Generated", ylabel = "Autocorrelation Coeff", xlabel = "Lag (time)", margin = 10Plots.mm)
 
-    ac_truth, lag, npairs = autocorr(timeseries, 1, 16, 16) # this is a portion of the timeseries if stride = 1
+    ac_truth, lag, npairs = autocorr(data_timeseries, 1, 16, 16) # this is a portion of the timeseries if stride = 1
     ac_truth_l = autocorr_inverse_cdf.(0.05, npairs, ac_truth)
     ac_truth_up = autocorr_inverse_cdf.(0.95, npairs, ac_truth)
     Plots.plot!(lag*dt_save, ac_truth, ribbon = (ac_truth .- ac_truth_l, ac_truth_up .- ac_truth), label = "Training")
     Plots.savefig(joinpath(savedir,"autocorr_samples.png"))
 
     # create plot showing distribution of spatial mean of generated and real images
-    spatial_mean_plot(timeseries, samples, savedir, "spatial_mean_distribution.png", logger=logger)
+    spatial_mean_plot(data_timeseries, gen_timeseries, savedir, "spatial_mean_distribution.png", logger=logger)
 
     # create q-q plot for cumulants of pre-specified scalar statistics
-    qq_plot(timeseries, samples, savedir, "qq_plot.png", logger=logger)
+    qq_plot(data_timeseries, gen_timeseries, savedir, "qq_plot.png", logger=logger)
 
     # create plots for comparison of real vs. generated spectra
-    spectrum_plot(timeseries, samples, savedir, "mean_spectra.png", logger=logger)
-    clims = extrema(timeseries)
+    spectrum_plot(data_timeseries, gen_timeseries, savedir, "mean_spectra.png", logger=logger)
+    clims = extrema(data_timeseries)
     frames = nsamples
     animation = @animate for i = 1:frames
-        heatmap(samples[:,:,1,i],
+        heatmap(gen_timeseries[:,:,1,i],
                 xaxis = false, yaxis = false, xticks = false, yticks = false,clims = clims,colorbar = :none
                 )
     end
     gif(animation, joinpath(savedir,"timeseries.gif"),fps=10)
 
     animation = @animate for i = 1:frames
-        heatmap(samples[:,:,1,i],
+        heatmap(gen_timeseries[:,:,1,i],
                 xaxis = false, yaxis = false, xticks = false, yticks = false,colorbar = :none
                 )
     end
@@ -123,13 +126,13 @@ function run_analysis(params; FT=Float32, logger=nothing)
     # get pure gen samples
     gen_images = Euler_Maruyama_sampler(model, init_x, time_steps, Δt; c=ctest[:,:,:,1:nsamples])
     # create plot showing distribution of spatial mean of generated and real images
-    spatial_mean_plot(timeseries, gen_images, savedir, "spatial_mean_distribution_images.png", logger=logger)
+    spatial_mean_plot(xtest[:,:,[1],1:nsamples], gen_images[:,:,[1],1:nsamples], savedir, "spatial_mean_distribution_images.png", logger=logger)
 
     # create q-q plot for cumulants of pre-specified scalar statistics
-    qq_plot(timeseries, gen_images, savedir, "qq_plot_images.png", logger=logger)
+    qq_plot(xtest[:,:,[1],1:nsamples], gen_images[:,:,[1],1:nsamples], savedir, "qq_plot_images.png", logger=logger)
 
     # create plots for comparison of real vs. generated spectra
-    spectrum_plot(timeseries, gen_images, savedir, "mean_spectra_images.png", logger=logger)
+    spectrum_plot(xtest[:,:,[1],1:nsamples], gen_images[:,:,[1],1:nsamples], savedir, "mean_spectra_images.png", logger=logger)
     loss_plot(savedir, "losses.png"; xlog = false, ylog = true)    
 end
 
