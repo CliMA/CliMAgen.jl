672d7cd0af65a27afe90d4edbaf46c9c9430568b
giorginiludovico <57105365+giorginiludovico@users.noreply.github.com>
2024-01-19 20:15:40 +0100

    periodic smooth perturbation

--- examples/giorgini2d/preliminary_plots.jl
+++ examples/giorgini2d/preliminary_plots.jl
@@ -1,14 +1,20 @@
 using GLMakie
 using HDF5
 using Distributions
+using LinearAlgebra
 
 function generate_2d_gaussian_lattice(N,mu,sigma)
-    x = range(1, stop=N, length=N)
-    y = range(1, stop=N, length=N)
-    gaussian_distribution = [pdf(MvNormal(mu, sigma), [xi, yi]) for xi in x, yi in y]
-    gaussian_distribution ./= sqrt(sum(gaussian_distribution.^2))
-    
-    return gaussian_distribution
+    center = (N+1) / 2
+    lattice = [exp(-0.5 * ([i, j] .- center)' * inv(sigma) * ([i, j] .- center)) for i in 1:N, j in 1:N]
+   
+    norm_factor = sqrt(sum(lattice .^ 2))
+    normalized_lattice = lattice / norm_factor
+
+    shift_x = Int(round(mod(mu[1] - center+0.5, N)))
+    shift_y = Int(round(mod(mu[2] - center+0.5, N)))
+    shifted_lattice = circshift(normalized_lattice, (shift_x, shift_y))
+
+    return shifted_lattice
 end
 
 function create_response_smooth(dx0,lags,R)
@@ -95,16 +101,16 @@ display(fig)
 
 ##
 N = 8
-mu = [0,0]
-sigma = [1,1]
+mu = [1 1]
+sigma = [1 0; 0 1]
 dx0 = generate_2d_gaussian_lattice(N,mu,sigma)
 x = range(1, stop=N, length=N)
 y = range(1, stop=N, length=N)
 
 fig = Figure()
 ax = Axis3(fig[1, 1], aspect = (1, 1, 1))
-surface!(ax, reverse(x), reverse(y), dx0', colormap = :viridis, rotation = pi/2)
-save("perturbation_$(sigma[1]).png", fig)
+surface!(ax, x, y, dx0, colormap = :viridis, rotation = pi/2)
+save("perturbation_$(sigma[1,1]).png", fig)
 
 scale = 0.75
 fig = Figure(resolution = (scale * 2000, scale * 750)) 
@@ -131,9 +137,9 @@ for (i, gl) in enumerate(glist)
     numerical_indices = read(hfile["lag_indices"])
     close(hfile)
 
-    lr_smooth = create_response_smooth(dx0,linear_indices,linear_response[:,1,:])[1:8:end,:] # effect of perturbation of pixel 1 on pixel i
-    srh_smooth = create_response_smooth(dx0,score_indices,score_response_hack[:,1,:])[1:8:end,:] # effect of perturbation of pixel 1 on pixel i
-    nr_smooth = create_response_smooth(dx0,numerical_indices,numerical_response[:,:])[1:8:end,1:4:end] # effect of perturbation of pixel 1 on pixel i
+    lr_smooth = create_response_smooth(dx0,linear_indices,linear_response[:,1,:])[1:8:end,1:27] # effect of perturbation of pixel 1 on pixel i
+    srh_smooth = create_response_smooth(dx0,score_indices,score_response_hack[:,1,:])[1:8:end,1:27] # effect of perturbation of pixel 1 on pixel i
+    nr_smooth = create_response_smooth(dx0,numerical_indices,numerical_response[:,:])[1:8:end,1:4:4*27] # effect of perturbation of pixel 1 on pixel i
     
     for j in 1:8
         ax = Axis(gl[1, j])
@@ -165,4 +171,5 @@ for (i, gl) in enumerate(glist)
     end
     Label(gl[1, 0], text = titles[i], fontsize = scale * 50, rotation = pi/2)
 end
-save("responses_smooth_$(sigma[1]).png", fig)
+save("responses_smooth_$(sigma[1,1]).png", fig)
+
