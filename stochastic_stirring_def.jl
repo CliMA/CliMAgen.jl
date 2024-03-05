f109d09076aaddf4394faba54980be7f0d44e419
Andre Souza <andrenogueirasouza@gmail.com>
2024-03-05 17:12:27 -0500

    update script to be 64x64

--- stochastic_stirring_def.jl
+++ stochastic_stirring_def.jl
@@ -81,14 +81,32 @@ function mock_callback(batch; ps = ps, opt = opt, lossfn = lossfn, ps_smooth = p
     return nothing
 end
 ##
-batch = zeros(FT, Nx, Ny, 1, t_sims);
-gradient_steps = 100
+batch = zeros(FT, Ny, Ny, 1, t_sims);
+gradient_steps = 200
 for j in ProgressBar(1:gradient_steps)
     for (i,simulation) in enumerate(sims)
         run!(simulation,period=Day(1),output=true)
-        batch[:, :, 1, i] .= FT.((simulation.model.output.vor .- μ) / σ)
+        tmp = FT.((simulation.model.output.vor .- μ) / σ)
+        rtmp = tmp[1:2:end, :] + tmp[2:2:end, :]
+        batch[:, :, 1, i] .= rtmp
         rm("run_0001", recursive=true) 
     end
     mock_callback(device(batch))
 end
 
+##
+nsamples = 1
+nsteps = 250
+resolution = (Ny, Ny)
+time_steps, Δt, init_x = setup_sampler(
+    model_smooth,
+    device,
+    resolution[1],
+    inchannels;
+    num_images=nsamples,
+    num_steps=nsteps,
+)
+
+samples = Euler_Maruyama_sampler(model_smooth, init_x, time_steps, Δt)
+
+heatmap(samples[:,:,1,1])
