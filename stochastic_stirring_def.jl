e6cf9b4bd1634760891e800af12f8ae81cfcc960
Andre Souza <andrenogueirasouza@gmail.com>
2024-03-05 17:21:01 -0500

    small update

--- stochastic_stirring_def.jl
+++ stochastic_stirring_def.jl
@@ -32,7 +32,8 @@ for (i,simulation) in enumerate(sims)
 end
 μ = mean(batch[:,:,1,:])
 σ = std(batch[:,:,1,:])
-sigmax =  maximum([norm(batch[:,:,1,i] - batch[:, :, 1, j]) for i in 1:t_sims, j in 1:t_sims if i != j]) / σ
+sigmax =  maximum([norm(batch[:,:,1,i] - batch[:, :, 1, j]) for i in 1:t_sims, j in 1:t_sims if i != j]) / σ 
+
 
 ##
 FT = Float32
@@ -83,6 +84,7 @@ end
 ##
 batch = zeros(FT, Ny, Ny, 1, t_sims);
 gradient_steps = 200
+sigmaxs = []
 for j in ProgressBar(1:gradient_steps)
     for (i,simulation) in enumerate(sims)
         run!(simulation,period=Day(1),output=true)
@@ -91,6 +93,8 @@ for j in ProgressBar(1:gradient_steps)
         batch[:, :, 1, i] .= rtmp
         rm("run_0001", recursive=true) 
     end
+    sigmax =  maximum([norm(batch[:,:,1,i] - batch[:, :, 1, j]) for i in 1:t_sims, j in 1:t_sims if i != j]) 
+    push!(sigmaxs , sigmax)
     mock_callback(device(batch))
 end
 
