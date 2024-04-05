a4186fd68c52caf92b668094263a52ada01aa588
sandreza <andrenogueirasouza@gmail.com>
2024-04-05 17:34:17 -0400

    prototypes

---
+++ sandbox/parallel_online_primitive_model.jl
@@ -0,0 +1,175 @@
+using LinearAlgebra
+using Statistics
+using ProgressBars
+using Flux
+using CliMAgen
+
+using Distributed
+using LinearAlgebra, Statistics
+
+if nworkers() < 8
+    addprocs(8)
+end
+
+@everywhere using Random
+@everywhere using SpeedyWeather
+@everywhere using StochasticStir
+@everywhere using SharedArrays
+
+@everywhere include("my_vorticity.jl")
+
+spectral_grid = SpectralGrid(trunc=31, nlev=5)
+my_vorticity_on_1 = MyInterpolatedVorticity(spectral_grid, schedule = Schedule(every=Day(1)))
+
+gated_array = SharedArray{spectral_grid.NF}(my_vorticity_on_1.interpolator.locator.npoints, Distributed.nworkers())
+gated_array .= 0.0
+# julia --project -p 8
+# open is true, closed is false. Open means we can write to the array
+gates = SharedVector{Bool}(nworkers())
+open_all!(gates) = gates .= true
+open_all!(gates)
+@everywhere all_closed(gates) = sum(gates) == 0
+@everywhere all_open(gates) = all(gates)
+@everywhere gate_open(gates, gate_id::Integer) = gates[gate_id]
+@everywhere close_gate!(gates, gate_id::Integer) = gates[gate_id] = false
+
+# Initialize Speedy Weather Parameters
+@info "initializing speedy weather sigma"
+if myid() == 1
+Random.seed!(1234) 
+
+# model
+ocean = AquaPlanet(spectral_grid, temp_equator=302, temp_poles=273)
+land_sea_mask = AquaPlanetMask(spectral_grid)
+orography = NoOrography(spectral_grid)
+model = PrimitiveWetModel(; spectral_grid, ocean) 
+model.feedback.verbose = false
+# callbacks
+my_vorticity = deepcopy(my_vorticity_on_1)
+add!(model.callbacks, my_vorticity)
+# run
+simulation = initialize!(model)
+run!(simulation, period=Day(30))
+tmp1 = copy(my_vorticity.var)
+run!(simulation, period=Day(30))
+tmp2 = copy(my_vorticity.var)
+# sigma max
+rtmp1 = reshape(tmp1, (128, 64))
+rtmp2 = reshape(tmp2, (128, 64))
+σ = quantile(abs.(rtmp2[:]), 0.9)
+sigmax = norm(rtmp1 - rtmp2) / σ * 1.2
+
+
+## Define Score-Based Diffusion Model
+@info "Defining Score Model"
+FT = Float32
+#Read in from toml
+batchsize = nworkers()
+inchannels = 1
+sigma_min = FT(1e-2);
+sigma_max = FT(sigmax);
+nwarmup = 5000;
+gradnorm = FT(1.0);
+learning_rate = FT(2e-4);
+beta_1 = FT(0.9);
+beta_2 = FT(0.999);
+epsilon = FT(1e-8);
+ema_rate = FT(0.999);
+device = Flux.gpu
+
+# Create network
+quick_arg = true
+net = NoiseConditionalScoreNetwork(;
+                                    noised_channels = inchannels,
+                                    shift_input = quick_arg,
+                                    shift_output = quick_arg,
+                                    mean_bypass = quick_arg,
+                                    scale_mean_bypass = quick_arg,
+                                    gnorm = quick_arg,
+                                    )
+score_model = VarianceExplodingSDE(sigma_max, sigma_min, net)
+score_model = device(score_model)
+score_model_smooth = deepcopy(score_model)
+opt = Flux.Optimise.Optimiser(WarmupSchedule{FT}(nwarmup),
+                              Flux.Optimise.ClipNorm(gradnorm),
+                              Flux.Optimise.Adam(learning_rate,(beta_1, beta_2), epsilon)
+) 
+opt_smooth = ExponentialMovingAverage(ema_rate);
+# model parameters
+ps = Flux.params(score_model);
+# setup smoothed parameters
+ps_smooth = Flux.params(score_model_smooth);
+lossfn = x -> score_matching_loss(score_model, x);
+function mock_callback(batch; ps = ps, opt = opt, lossfn = lossfn, ps_smooth = ps_smooth, opt_smooth = opt_smooth)
+    grad = Flux.gradient(() -> sum(lossfn(batch)), ps)
+    Flux.Optimise.update!(opt, ps, grad)
+    Flux.Optimise.update!(opt_smooth, ps_smooth, ps)
+    return nothing
+end
+
+end # myid() == 1
+##
+@info "Done Defining score model"
+# Run Models
+nsteps = 10000 # 10000
+const SLEEP_DURATION = 1e-3
+
+@distributed for i in workers()
+    id = myid()
+    gate_id = id-1
+    Random.seed!(1234+id)
+    # model
+    ocean = AquaPlanet(spectral_grid, temp_equator=302, temp_poles=273)
+    land_sea_mask = AquaPlanetMask(spectral_grid)
+    orography = NoOrography(spectral_grid)
+    model = PrimitiveWetModel(; spectral_grid, ocean)
+    model.feedback.verbose = false
+    # callbacks
+    my_vorticity = deepcopy(my_vorticity_on_1)
+    add!(model.callbacks, my_vorticity)
+    # initialize and run
+    simulation = initialize!(model)
+    run!(simulation, period=Day(30))
+    
+    for _ in 1:nsteps
+        run!(simulation, period=Day(1))
+        gate_written::Bool = false
+        while ~gate_written
+            if gate_open(gates, gate_id)
+                gated_array[:, gate_id] .= my_vorticity.var
+                close_gate!(gates, gate_id)
+                # println("Closing gate $gate_id")
+                gate_written = true
+            else
+                # println("Gate $gate_id closed, sleep.")
+                sleep(SLEEP_DURATION)
+            end
+        end
+    end
+    println("FINISHED.")
+end
+
+# HAPPENING ON PROC 1
+if myid() == 1
+    tic = Base.time()
+    j = Ref(1)      # needs to be mutable somehow
+    while j[] <= nsteps
+        if all_closed(gates)
+            j[] += 1
+            # println(gated_array[1, :])
+            println(j)
+            rbatch = copy(reshape(gated_array, (128, 64, 1, batchsize)))
+            batch = rbatch / (2σ)
+            open_all!(gates)
+            mock_callback(device(batch))
+            # mock_callback(device(batch))
+            # println("All gates opened.")
+        else
+            # println("Gates still open: $gates")
+            sleep(SLEEP_DURATION)
+        end
+    end
+end
+
+toc = Base.time()
+println("Time for the simulation is $((toc-tic)/60) minutes.")
--- sandbox/primitive_model_speedy_weather.jl
+++ sandbox/primitive_model_speedy_weather.jl
@@ -1,19 +1,17 @@
 using SpeedyWeather
 include("my_field.jl")
 # components
-spectral_grid = SpectralGrid(trunc=31, nlev=5) # SpectralGrid(trunc=31, nlev=5)
+spectral_grid = SpectralGrid(trunc=40, nlev=8) # SpectralGrid(trunc=31, nlev=5)
 ocean = AquaPlanet(spectral_grid, temp_equator=302, temp_poles=273)
 land_sea_mask = AquaPlanetMask(spectral_grid)
 orography = NoOrography(spectral_grid)
-
-# create model, initialize, run
 model = PrimitiveWetModel(; spectral_grid, ocean) # , land_sea_mask, orography)
 simulation = initialize!(model)
 # :nlat_half, :vor_grid, :div_grid,
 #  :temp_grid, :temp_grid_prev, :temp_virt_grid, :humid_grid, 
 # :u_grid, :v_grid, :u_grid_prev, :v_grid_prev
 my_fields = []
-for field in [:temp_grid, :vor_grid]
+for field in [:temp_grid, :vor_grid, :humid_grid, :div_grid]
     my_field_on_1 = MyInterpolatedField(spectral_grid; schedule = Schedule(every=Day(1)), field_name = field)
     my_field = deepcopy(my_field_on_1)
     push!(my_fields, my_field)
@@ -28,4 +26,8 @@ lat, lon = RingGrids.get_latdlonds(my_fields[1].var)
 heatmap!(ax, reshape(lon, (128, 64))[:,1], reshape(lat, (128, 64))[1, :], reshape(my_fields[1].var, (128, 64)))
 ax = Axis(fig[1, 2]; title = "Vorticity")
 heatmap!(ax, reshape(lon, (128, 64))[:,1], reshape(lat, (128, 64))[1, :], reshape(my_fields[2].var, (128, 64)))
+ax = Axis(fig[2, 1]; title = "Humidity")
+heatmap!(ax, reshape(lon, (128, 64))[:,1], reshape(lat, (128, 64))[1, :], reshape(my_fields[3].var, (128, 64)))
+ax = Axis(fig[2, 2]; title = "Divergence")
+heatmap!(ax, reshape(lon, (128, 64))[:,1], reshape(lat, (128, 64))[1, :], reshape(my_fields[4].var, (128, 64)))
 save("fields.png", fig)
