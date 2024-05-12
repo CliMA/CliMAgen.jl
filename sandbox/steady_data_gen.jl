d705bd904e925e85fdf600fd91ed56e6c2585ee7
sandre <sandre@eofe9.inband>
2024-05-12 15:51:28 -0400

    add hdf5 and update scripts

--- sandbox/steady_data_gen.jl
+++ sandbox/steady_data_gen.jl
@@ -10,6 +10,7 @@ using Random
 using SpeedyWeather
 using StochasticStir
 using SharedArrays
+using HDF5
 
 
 include("my_field.jl")
@@ -53,18 +54,54 @@ simulation = initialize!(model)
 run!(simulation, period=Day(100))
 @info "Gather Timeseries"
 steps = 4000
+skip_days = 14
 timeseries = zeros(spectral_grid.NF, my_fields[1].interpolator.locator.npoints, length(my_fields), steps)
 for j in ProgressBar(1:steps)
-    run!(simulation, period=Day(14))
+    run!(simulation, period=Day(skip_days))
     for (i, my_field) in enumerate(my_fields)
         timeseries[:, i, j] = copy(my_fields[i].var)
     end
 end
 
 n_fields = length(my_fields)
-r_timeseries = reshape(timeseries, (128, 64, n_fields, steps)) 
+r_timeseries = copy(reshape(timeseries, (128, 64, n_fields, steps)) )
 μ = mean(r_timeseries, dims = (1, 2, 4))
 μ_drift = mean(r_timeseries, dims = (1, 2))
-σ = reshape([quantile(abs.(r_timeseries[:, :, i, :] .- μ)[:], 0.99) for i in 1:n_fields], (1, 1, n_fields, 1))
+σ = reshape([quantile(abs.(r_timeseries[:, :, i, :] .- μ)[:], 0.999) for i in 1:n_fields], (1, 1, n_fields, 1))
 
 rescaled_timeseries = (r_timeseries .- μ) ./ σ
+
+shuffled_steps = shuffle(1:steps)
+N = 100
+shuffled_steps_1 = shuffled_steps[1:N]
+shuffled_steps_2 = shuffled_steps[end-N+1:end]
+distances = [norm(rescaled_timeseries[:,:,:,i]-rescaled_timeseries[:,:,:,j]) for i in shuffled_steps_1, j in shuffled_steps_2]
+sigmax = maximum(distances) * 1.2
+##
+hfile = h5open("steady_data.hdf5", "w")
+hfile["timeseries"] = rescaled_timeseries
+hfile["shift"] = μ
+hfile["scaling"] = σ
+hfile["sigmax"] = sigmax
+close(hfile)
+
+##
+@info "Gather Timeseries Part 2"
+timeseries = zeros(spectral_grid.NF, my_fields[1].interpolator.locator.npoints, length(my_fields), steps)
+for j in ProgressBar(1:steps)
+    run!(simulation, period=Day(skip_days))
+    for (i, my_field) in enumerate(my_fields)
+        timeseries[:, i, j] = copy(my_fields[i].var)
+    end
+end
+
+n_fields = length(my_fields)
+r_timeseries = copy(reshape(timeseries, (128, 64, n_fields, steps)) )
+rescaled_timeseries = (r_timeseries .- μ) ./ σ
+##
+hfile = h5open("steady_data_2.hdf5", "w")
+hfile["timeseries"] = rescaled_timeseries
+hfile["shift"] = μ
+hfile["scaling"] = σ
+hfile["sigmax"] = sigmax
+close(hfile)
---
+++ sandbox/together.jl
@@ -0,0 +1,2 @@
+include("steady_data_gen.jl")
+include("steady_state_after_gen.jl")
