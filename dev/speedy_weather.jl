8bce6264c66d981efb1a9186c2ad55af18ce3365
sandre <sandre@eofe9.inband>
2024-05-14 15:53:32 -0400

    speedy weather update

---
+++ dev/speedy_weather.jl
@@ -0,0 +1,92 @@
+using SpeedyWeather
+using Distributions    # just for Uniform(a,b)
+using Random
+# Random.seed!(12345)
+
+spectral_grid = SpectralGrid(trunc=31, nlev=5)
+
+parameters = (;
+rotation = rand(Uniform(-1e-4, 1e-4)),
+
+relax_time_slow = Second(round(Int, 24*3600*rand(Uniform(20,80)))),
+relax_time_fast = Second(round(Int, 24*3600*rand(Uniform(2,8)))),
+Tmin = rand(Uniform(190,210)),
+Tmax = rand(Uniform(305,325)),
+ΔTy = rand(Uniform(50,70)),
+Δθz = rand(Uniform(5,15)),
+
+time_scale = Second(round(Int, 3600*rand(Uniform(2,16)))),
+relative_humidity = rand(Uniform(0.6,0.8)),
+
+f_wind = rand(Uniform(0,1)),
+V_gust = rand(Uniform(0,10)),
+
+orography_scale = rand(Uniform(0,1))
+)
+
+default_parameters = (; 
+rotation = 7.29e-5, 
+
+relax_time_slow = 40*Day(1),
+relax_time_fast = 4*Day(1),
+Tmin = 200,
+Tmax = 315,
+ΔTy = 60,
+Δθz = 10,
+
+time_scale = 4*Hour(1),
+relative_humidity = 0.7,
+
+f_wind = 0.95,
+V_gust = 5,
+
+orography_scale = 1
+)
+
+model = PrimitiveWetModel(;
+    spectral_grid,
+
+    planet = Earth(spectral_grid,
+        rotation=parameters.rotation, # default 7.29e-5 1/s
+    ),
+
+    # Held-Suarez forcing but not its drag
+    temperature_relaxation = HeldSuarez(spectral_grid,
+        relax_time_slow=parameters.relax_time_slow,  # default 40 days
+        relax_time_fast=parameters.relax_time_fast,  # default 4 days
+        Tmin=parameters.Tmin,           # default 200K
+        Tmax=parameters.Tmax,           # default 315K
+        ΔTy=parameters.ΔTy,             # default 60K
+        Δθz=parameters.Δθz,             # default 10K
+    ),
+
+    # other physics
+    convection = SimplifiedBettsMiller(spectral_grid,
+        time_scale= parameters.time_scale,                # default 4 hours
+        relative_humidity=parameters.relative_humidity,   # default 0.7
+    ),
+    large_scale_condensation = ImplicitCondensation(spectral_grid),
+    shortwave_radiation = NoShortwave(),
+    longwave_radiation = NoLongwave(),
+    vertical_diffusion = NoVerticalDiffusion(),
+    # vertical_diffusion = BulkRichardsonDiffusion(spectral_grid), # maybe needed
+
+    # Surface fluxes
+    boundary_layer_drag = BulkRichardsonDrag(spectral_grid),
+    surface_wind = SurfaceWind(spectral_grid,
+        f_wind= parameters.f_wind,          # surface wind scale, default 0.95
+        V_gust= parameters.V_gust,         # wind gusts, default 5m/s
+    ),
+
+    # use Earth's orography
+    orography = EarthOrography(spectral_grid),
+)
+
+simulation = initialize!(model)
+
+# scale orography manually
+orography_scale = parameters.orography_scale
+model.orography.orography .*= orography_scale
+model.orography.geopot_surf .*= orography_scale
+
+run!(simulation, period=Day(100))
